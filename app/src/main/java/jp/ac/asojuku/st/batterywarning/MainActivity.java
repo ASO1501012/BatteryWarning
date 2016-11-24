ccebb587ce4e9ddbe34be041ce098b925f9c8cef
Garudo24 <1501012@st.asojuku.ac.jp>
2016-11-24 10:18:53 +0900

    commit

--- app/src/main/java/jp/ac/asojuku/st/batterywarning/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/st/batterywarning/MainActivity.java
@@ -1,6 +1,5 @@
 package jp.ac.asojuku.st.batterywarning;
 
-import android.app.Activity;
 import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,15 +10,9 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private MyBroadcastReceiver mReceiver;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +35,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class MyBroadcastReceiver extends BroadcastReceiver {
-
         @Override
         public void onReceive(Context context, Intent intent){
-
             if(intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)){
                 int scale = intent.getIntExtra("scale",0);
                 int level = intent.getIntExtra("level",0);
@@ -70,32 +61,17 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Notification.Builder builder = new Notification.Builder(getApplicationContext());
                 builder.setSmallIcon(R.drawable.icon);
-
-                builder.setContentTitle("バッテリー監視"); // 1行目
-                builder.setContentText("バッテリーが15%以下です！"); // 2行目
-
-                builder.setTicker("Battery Warning"); // 通知到着時に通知バーに表示(4.4まで)
-// 5.0からは表示されない
+                builder.setContentTitle("バッテリー監視");
+                builder.setContentText("バッテリーが15%以下です！");
+                builder.setTicker("Battery Warning");
 
                 NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-
                 String title = "Battery Watch";
                 String msg = statusString + " " + level + "/" + scale;
                 Log.v(title, msg);
-
-
-                if(level <= 20){
+                if(level <= 15){
                     manager.notify(1, builder.build());
                 }
-
-                Activity mainActivity = (Activity)context;
-                TextView tvtitle =  (TextView) mainActivity.findViewById(R.id.tvtitle);
-                tvtitle.setText(title);
-
-                TextView tvMsg = (TextView) mainActivity.findViewById(R.id.tvMsg);
-                tvMsg.setText(msg);
-
-                Toast.makeText(mainActivity,msg,Toast.LENGTH_SHORT).show();
             }
         }
     };
